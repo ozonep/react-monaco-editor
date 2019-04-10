1d084c9d2de12e845e063a312d08e53658c1b481
ozonep <ozonep@yandex.ru>
2019-04-10 12:33:13 +0300

    changes to example

--- example/index.js
+++ example/index.js
@@ -1,51 +1,42 @@
 /* eslint-disable */
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import {MonacoEditor, MonacoDiffEditor} from 'react-monaco-hooks';
 
 
-class CodeEditor extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      code: '// type your code... \n',
-    }
-  }
+function CodeEditor(props) {
+ const [code, setCode] = useState('// type your code... \n');
+ let editor;
 
-  onChange = (newValue, e) => {
-    console.log('onChange', newValue, e); // eslint-disable-line no-console
-  }
+ const onChange = (newValue, e) => {
+   console.log('onChange', newValue, e);
+ };
 
-  editorDidMount = (editor) => {
-    // eslint-disable-next-line no-console
-    console.log('editorDidMount', editor, editor.getValue(), editor.getModel());
-    this.editor = editor;
-  }
+  const editorDidMount = (editorr) => {
+    console.log('editorDidMount', editorr, editorr.getValue(), editorr.getModel());
+    editor = editorr;
+  };
 
-  changeEditorValue = () => {
-    if (this.editor) {
-      this.editor.setValue('// code changed! \n');
-    }
-  }
+  const changeEditorValue = () => {
+    if (editor) editor.setValue('// code changed! \n');
+  };
 
-  changeBySetState = () => {
-    this.setState({ code: '// code changed by setState! \n' });
-  }
+  const changeBySetState = () => {
+    setCode('// code changed by setState! \n');
+  };
 
-  render() {
-    const { code } = this.state;
-    const options = {
-      selectOnLineNumbers: true,
-      roundedSelection: false,
-      readOnly: false,
-      cursorStyle: 'line',
-      automaticLayout: false,
-    };
-    return (
+  const options = {
+    selectOnLineNumbers: true,
+    roundedSelection: false,
+    readOnly: false,
+    cursorStyle: 'line',
+    automaticLayout: false,
+  };
+  return (
       <div>
         <div>
-          <button onClick={this.changeEditorValue} type="button">Change value</button>
-          <button onClick={this.changeBySetState} type="button">Change by setState</button>
+          <button onClick={changeEditorValue} type="button">Change value</button>
+          <button onClick={changeBySetState} type="button">Change by setState</button>
         </div>
         <hr />
         <MonacoEditor
@@ -53,72 +44,29 @@ class CodeEditor extends React.Component {
           language="javascript"
           value={code}
           options={options}
-          onChange={this.onChange}
-          editorDidMount={this.editorDidMount}
+          onChange={onChange}
+          theme="vs-dark"
+          editorDidMount={editorDidMount}
         />
       </div>
     );
-  }
 }
 
-class AnotherEditor extends React.Component { // eslint-disable-line react/no-multi-comp
-  constructor(props) {
-    super(props);
-    const jsonCode = [
-      '{',
-      '    "$schema": "http://myserver/foo-schema.json"',
-      '}'
-    ].join('\n');
-    this.state = {
-      code: jsonCode,
-    }
-  }
-
-  editorWillMount = (monaco) => {
-    monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
-      validate: true,
-      schemas: [{
-        uri: 'http://myserver/foo-schema.json',
-        schema: {
-          type: 'object',
-          properties: {
-            p1: {
-              enum: [ 'v1', 'v2']
-            },
-            p2: {
-              $ref: 'http://myserver/bar-schema.json'
-            }
-          }
-        }
-      }, {
-        uri: 'http://myserver/bar-schema.json',
-        schema: {
-          type: 'object',
-          properties: {
-            q1: {
-              enum: [ 'x1', 'x2']
-            }
-          }
-        }
-      }]
-    });
-  }
-
-  render() {
-    const { code } = this.state;
-    return (
+function AnotherEditor(props) {
+  const code1 = "// your original code...";
+  const code2 = "// a different version...";
+  return (
       <div>
         <MonacoDiffEditor
           width="800"
           height="600"
-          language="json"
-          defaultValue={code}
+          language="javascript"
           theme="vs-dark"
-          editorWillMount={this.editorWillMount}
+          original={code1}
+          value={code2}
         />
       </div>
     );
-  }
 }
 
 const App = () => (
@@ -129,7 +77,7 @@ const App = () => (
     <h2>Another editor (uncontrolled mode)</h2>
     <AnotherEditor />
   </div>
-)
+);
 
 ReactDOM.render(
   <App />,
